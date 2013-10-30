b4dceefa264e4317c76e4d52e929cc5c16c36837
arpydecal2 <sistemes@gisce.net>
2013-10-30 13:30:39 +0100

    Upgrade to v2.34.0

--- db_backup.sh
+++ db_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION="2.28"
+VERSION="2.34.0"
 for DBNAME in `psql -ltA | grep '|erp|' | cut -d '|' -f 1`; do
   echo "Dumping $DBNAME" && \
   mkdir -p /home/erp/var/backups/${DBNAME} && \
--- update_server.sh
+++ update_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /home/erp/src/erp/server/bin
-for SCONF in `ls -1 /home/erp/conf`; do
+for SCONF in `ls -1 /home/erp/conf | grep -v mongo`; do
   echo "Updating $SCONF..." && \
   ./openerp-server.py --config=/home/erp/conf/$SCONF --price_accuracy=6 --update=all --stop-after-init && \
   echo "******* $SCONF Done ********" &
